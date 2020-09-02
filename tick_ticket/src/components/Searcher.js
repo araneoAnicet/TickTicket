f5bd1476431bf2bb565a881ab0051b2dba283c11
araneoAnicet <araneoanicet@gmail.com>
2020-09-02 15:22:35 +0300

    add Date button

--- tick_ticket/src/components/Searcher.js
+++ tick_ticket/src/components/Searcher.js
@@ -2,22 +2,35 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import '../style.css'
+import Button from 'react-bootstrap/Button';
+import '../style.css';
+
 
 function Searcher(props) {
     return (
         <div className="edges-margin">
             {props.children}
-            <Form>
-            <Row>
-                <Col>
-                <Form.Control placeholder="From.." />
-                </Col>
-                <Col>
-                <Form.Control placeholder="To.." />
-                </Col>
-            </Row>
-            </Form>
+
+                <Form>
+                <Row>
+                    <Col>
+                    <Form.Control placeholder="From.." />
+                    <Form.Text className="text-muted">
+                        From
+                    </Form.Text>
+                    </Col>
+                    <Col>
+                    <Form.Control placeholder="To.." />
+                    <Form.Text className="text-muted">
+                        To
+                    </Form.Text>
+                    </Col>
+                    <Col>
+                    <Button variant="outline-dark" block>Date</Button>
+                    </Col>
+                </Row>
+                </Form>
+
         </div>
     );
 }
