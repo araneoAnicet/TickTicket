2daca9eda668ddcae4f88d1ddd77f5a23b9d852c
araneoAnicet <araneoanicet@gmail.com>
2020-09-17 22:06:17 +0100

    add Total Price and Buy all

--- tick_ticket/src/components/MyTickets.js
+++ tick_ticket/src/components/MyTickets.js
@@ -89,6 +89,43 @@ function MyTickets(props, ref) {
         );
     }
 
+    function buyAllTicketsComponent() {
+        if (props.tickets.size !== 0) {
+            var totalPrice = 0;
+            for (var ticket of props.tickets) {
+                totalPrice += ticket.price;
+            }
+            return (
+                <Container style={{ marginTop: '2em' }}>
+                    <Row>
+                        <Col/>
+                        <Col>
+                            <h4>
+                                Total price:
+                            </h4>
+                        </Col>
+                        <Col>
+                            <h4>
+                                <span className="text-danger">{totalPrice} </span> <span>{props.tickets.values().next().value.currencyName}</span>
+                            </h4>
+                        </Col>
+                        <Col>
+                            <Button size="lg" variant="danger">
+                                Buy all
+                                <svg style={{ marginLeft: '0.5em' }} width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-cash-stack" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14 3H1a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1h-1z"/>
+                                <path fillRule="evenodd" d="M15 5H1v8h14V5zM1 4a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H1z"/>
+                                <path d="M13 5a2 2 0 0 0 2 2V5h-2zM3 5a2 2 0 0 1-2 2V5h2zm10 8a2 2 0 0 1 2-2v2h-2zM3 13a2 2 0 0 0-2-2v2h2zm7-4a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
+                                </svg>
+                            </Button>
+                        </Col>
+                    </Row>
+                </Container>
+            );
+        }
+        return null;
+    }
+
 
     return (
         <Container style={{ marginTop: '2em' }} ref={ref}>
@@ -112,6 +149,7 @@ function MyTickets(props, ref) {
                     </Nav>
                     {cartMessageComponent()}
                     {cartTicketsComponent()}
+                    {buyAllTicketsComponent()}
                 </Jumbotron>
             </Col>
             <Col md={3} lg={3}/>
