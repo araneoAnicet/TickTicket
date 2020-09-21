39fd37057c14978a437ce5609d7c810162c1b34e
araneoAnicet <araneoanicet@gmail.com>
2020-09-21 12:40:57 +0100

    add Carrier model

--- tick_ticket/tick_ticket_site/tick_ticket/models.py
+++ tick_ticket/tick_ticket_site/tick_ticket/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 class City(models.Model):
     name = models.CharField(max_length=85)
+
+class Carrier(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=360)
+    icon = models.ImageField(upload_to='carrier_icons/')
