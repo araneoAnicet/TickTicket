63d52f7d8a47b65c6b640ab84c986a9933d3f1bf
araneoAnicet <araneoanicet@gmail.com>
2020-09-21 13:14:19 +0100

    add Ticket model

--- tick_ticket/tick_ticket_site/tick_ticket/admin.py
+++ tick_ticket/tick_ticket_site/tick_ticket/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import City
+from .models import City, Carrier, Ticket
 
 admin.site.register(City)
+admin.site.register(Carrier)
+admin.site.register(Ticket)
--- tick_ticket/tick_ticket_site/tick_ticket/models.py
+++ tick_ticket/tick_ticket_site/tick_ticket/models.py
@@ -7,3 +7,15 @@ class Carrier(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=360)
     icon = models.ImageField(upload_to='carrier_icons/')
+
+class Ticket(models.Model):
+    departure_time = models.TimeField()
+    arrive_time = models.TimeField()
+    departure_date = models.DateField()
+    arrive_date = models.DateField()
+    departure_city = models.ForeignKey('City', on_delete=models.CASCADE, related_name='departure_tickets')
+    arrive_city = models.ForeignKey('City', on_delete=models.CASCADE, related_name='arrive_tickets')
+    carrier = models.ForeignKey('Carrier', on_delete=models.CASCADE, related_name='tickets')
+    price = models.FloatField(default=0)
+    currency_name = models.CharField(max_length=3, default='USD')
+ 
