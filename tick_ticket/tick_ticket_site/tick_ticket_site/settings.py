8469f6b7a925639e54984752e369b2905933757e
araneoAnicet <araneoanicet@gmail.com>
2020-09-22 13:43:40 +0100

    add viewsets for REST

--- tick_ticket/tick_ticket_site/tick_ticket_site/settings.py
+++ tick_ticket/tick_ticket_site/tick_ticket_site/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tick_ticket'
+    'tick_ticket',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
