4bafef854a82d021831ba3204715225de47d14e0
araneoAnicet <araneoanicet@gmail.com>
2020-09-26 17:40:57 +0100

    add SearchTickets view set

--- tick_ticket/tick_ticket_site/tick_ticket/views.py
+++ tick_ticket/tick_ticket_site/tick_ticket/views.py
@@ -9,6 +9,26 @@ class TicketsViewSet(viewsets.ModelViewSet):
     serializer_class = TicketSerializer
     queryset = Ticket.objects.all()
 
+class SearchersViewSet(viewsets.ViewSet):
+    def list(self, request):
+        query = Ticket.objects.all()
+        departur_city = request.data.get('departureCity')
+        arrive_city = request.data.get('arriveCity')
+        departure_date = request.data.get('departureDate')
+        arrive_date = request.data.get('arriveDate')
+        is_round_trip = request.data.get('isRoundTrip')
+
+        if departur_city:
+            query = query.filter(departur_city=departur_city)
+        if arrive_city:
+            query = query.filter(arrive_city=arrive_city)
+        if departure_date:
+            query = query.filter(departur_date=departur_date)
+        if arrive_date:
+            query = query.fitler(arrive_date=arrive_date)
+        
+        serializer = TicketSerializer(query, many=True)
+        return Response(serializer.data)
 
 class CitiesViewSet(viewsets.ViewSet):
     def list(self, request):
