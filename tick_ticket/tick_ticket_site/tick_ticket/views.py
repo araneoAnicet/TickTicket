b15eed816f6a139ac04bccbeaa2c08e4afdb5f58
araneoAnicet <araneoanicet@gmail.com>
2020-09-26 18:26:51 +0100

    add filters for SearchersViewSet

--- tick_ticket/tick_ticket_site/tick_ticket/views.py
+++ tick_ticket/tick_ticket_site/tick_ticket/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer, CitySerializer, CarrierSerializer, TicketSerializer
 from .models import User, City, Carrier, Ticket
+from django.db.models import Q
 
 
 class TicketsViewSet(viewsets.ModelViewSet):
@@ -14,18 +15,21 @@ class SearchersViewSet(viewsets.ViewSet):
         query = Ticket.objects.all()
         departur_city = request.data.get('departureCity')
         arrive_city = request.data.get('arriveCity')
-        departure_date = request.data.get('departureDate')
-        arrive_date = request.data.get('arriveDate')
+        trip_date = request.data.get('tripDate')
         is_round_trip = request.data.get('isRoundTrip')
+        round_trip_date = request.data.get('roundTripDate')
 
-        if departur_city:
-            query = query.filter(departur_city=departur_city)
-        if arrive_city:
-            query = query.filter(arrive_city=arrive_city)
-        if departure_date:
-            query = query.filter(departur_date=departur_date)
-        if arrive_date:
-            query = query.fitler(arrive_date=arrive_date)
+        if is_round_trip:
+            if departur_city:
+                query = query.filter(Q(departur_city=departur_city) | Q(departur_city=arrive_city))
+            if arrive_city:
+                query = query.filter(Q(arrive_city=arrive_city) | Q(arrive_city=departur_city))
+            if trip_date:
+                query = query.filter(depature_date=trip_date)
+            elif round_trip_date:
+                query = query.filter(depature_date=round_trip_date)
+            elif trip_date and round_trip_date:
+                query = query.filter(Q(depature_date=trip_date) | Q(depature_date=round_trip_date))
         
         serializer = TicketSerializer(query, many=True)
         return Response(serializer.data)
