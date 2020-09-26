6f949c47aa2b13b23f9965c3a296ef74949bf06e
araneoAnicet <araneoanicet@gmail.com>
2020-09-26 18:31:19 +0100

    add filters for one-way trips

--- tick_ticket/tick_ticket_site/tick_ticket/views.py
+++ tick_ticket/tick_ticket_site/tick_ticket/views.py
@@ -13,24 +13,31 @@ class TicketsViewSet(viewsets.ModelViewSet):
 class SearchersViewSet(viewsets.ViewSet):
     def list(self, request):
         query = Ticket.objects.all()
-        departur_city = request.data.get('departureCity')
+        departure_city = request.data.get('departureCity')
         arrive_city = request.data.get('arriveCity')
         trip_date = request.data.get('tripDate')
         is_round_trip = request.data.get('isRoundTrip')
         round_trip_date = request.data.get('roundTripDate')
 
         if is_round_trip:
-            if departur_city:
-                query = query.filter(Q(departur_city=departur_city) | Q(departur_city=arrive_city))
+            if departure_city:
+                query = query.filter(Q(departure_city=departure_city) | Q(departure_city=arrive_city))
             if arrive_city:
-                query = query.filter(Q(arrive_city=arrive_city) | Q(arrive_city=departur_city))
+                query = query.filter(Q(arrive_city=arrive_city) | Q(arrive_city=departure_city))
             if trip_date:
                 query = query.filter(depature_date=trip_date)
             elif round_trip_date:
                 query = query.filter(depature_date=round_trip_date)
             elif trip_date and round_trip_date:
                 query = query.filter(Q(depature_date=trip_date) | Q(depature_date=round_trip_date))
-        
+        else:
+            if departure_city:
+                query = query.filter(departure_city=departure_city)
+            if arrive_city:
+                query = query.filter(arrive_city=arrive_city)
+            if trip_date:
+                query = query.filter(departure_date=departure_date)
+            
         serializer = TicketSerializer(query, many=True)
         return Response(serializer.data)
 
